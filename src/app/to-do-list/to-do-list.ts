2e70954f3c0cb866a6b028d74ad96998d6ae7be7
proximalbeet <nake73a@gmail.com>
2026-02-08 18:09:34 -0500

    Added a new component and created a angular project

---
+++ src/app/to-do-list/to-do-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list',
+  imports: [],
+  templateUrl: './to-do-list.html',
+  styleUrl: './to-do-list.css',
+})
+export class ToDoList {
+
+}
